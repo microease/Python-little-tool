29191a4d7c240178ce3dfc720a2a235c99b34a02
浮生若梦 <microease@163.com>
2019-10-11 00:16:47 +0800

    add

--- 自动加水印/marker.py
+++ 自动加水印/marker.py
@@ -9,7 +9,7 @@ import math
 import glob, os
 from PIL import Image, ImageFont, ImageDraw, ImageEnhance, ImageChops
 
-TTF_FONT = u'./font/青鸟华光简琥珀.ttf'
+TTF_FONT = u'./font/bird.ttf'
 
 
 def add_mark(imagePath, mark, args):
@@ -122,32 +122,32 @@ def gen_mark(args):
 def main():
     path = r'D:\Python-little-tool\自动加水印\input'
     files = glob.glob(os.path.join(path, "*.*"))
-    parse = argparse.ArgumentParser()
-    
-    parse.add_argument("-f", "--file", type=str, help="image file path or directory")
-    parse.add_argument("-m", "--mark", type=str, help="watermark content")
-    parse.add_argument("-o", "--out", default="./output", help="image output directory, default is ./output")
-    parse.add_argument("-c", "--color", default="#8B8B1B", type=str,
-                       help="text color like '#000000', default is #8B8B1B")
-    parse.add_argument("-s", "--space", default=100, type=int, help="space between watermarks, default is 75")
-    parse.add_argument("-a", "--angle", default=30, type=int, help="rotate angle of watermarks, default is 30")
-    parse.add_argument("--size", default=50, type=int, help="font size of text, default is 50")
-    parse.add_argument("--opacity", default=0.15, type=float, help="opacity of watermarks, default is 0.15")
+    for i in files:
+        parse = argparse.ArgumentParser()
+        parse.add_argument("-f", "--file", default=i, type=str, help="image file path or directory")
+        parse.add_argument("-m", "--mark", default="深圳慧商智选网络科技有限公司", type=str, help="watermark content")
+        parse.add_argument("-o", "--out", default="./output", help="image output directory, default is ./output")
+        parse.add_argument("-c", "--color", default="#FFFFFF", type=str,
+                           help="text color like '#000000', default is #8B8B1B")
+        parse.add_argument("-s", "--space", default=150, type=int, help="space between watermarks, default is 75")
+        parse.add_argument("-a", "--angle", default=30, type=int, help="rotate angle of watermarks, default is 30")
+        parse.add_argument("--size", default=50, type=int, help="font size of text, default is 50")
+        parse.add_argument("--opacity", default=0.1, type=float, help="opacity of watermarks, default is 0.15")
 
-    args = parse.parse_args()
+        args = parse.parse_args()
 
-    if isinstance(args.mark, str) and sys.version_info[0] < 3:
-        args.mark = args.mark.decode("utf-8")
+        if isinstance(args.mark, str) and sys.version_info[0] < 3:
+            args.mark = args.mark.decode("utf-8")
 
-    mark = gen_mark(args)
+        mark = gen_mark(args)
 
-    if os.path.isdir(args.file):
-        names = os.listdir(args.file)
-        for name in names:
-            image_file = os.path.join(args.file, name)
-            add_mark(image_file, mark, args)
-    else:
-        add_mark(args.file, mark, args)
+        if os.path.isdir(args.file):
+            names = os.listdir(args.file)
+            for name in names:
+                image_file = os.path.join(args.file, name)
+                add_mark(image_file, mark, args)
+        else:
+            add_mark(args.file, mark, args)
 
 
 if __name__ == '__main__':
