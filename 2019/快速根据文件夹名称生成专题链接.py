cb6323a735396f62510935b86a3989576cae6fcb
浮生若梦 <microease@163.com>
2019-10-18 16:55:31 +0800

    add

--- 2019/快速根据文件夹名称生成专题链接.py
+++ 2019/快速根据文件夹名称生成专题链接.py
@@ -7,8 +7,10 @@ str1 = "http://m.szffmr.com/zhuanti/"
 str2 = "http://mm.szffmr.com/zhuanti/"
 str3 = "http://m.88833222.com/zhuanti/"
 str4 = "http://m.ffyy.cc/zhuanti/"
+str5 = "http://wap.mr91.com/zhuanti/"
 dir_name = input("请输入文件夹名：")
 print(str1 + dir_name + "/")
 print(str2 + dir_name + "/")
 print(str3 + dir_name + "/")
 print(str4 + dir_name + "/")
+print(str5 + dir_name + "/")
