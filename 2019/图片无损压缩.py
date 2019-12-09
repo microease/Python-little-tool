4e0f06c99c6962e854f5c289fea97d5b97405cc6
浮生若梦 <microease@163.com>
2019-12-09 18:07:06 +0800

    Update 图片无损压缩.py

--- 2019/图片无损压缩.py
+++ 2019/图片无损压缩.py
@@ -9,7 +9,7 @@ tinify.key = 'SHhhzPb7lNm01Gr74j2TGmfxyDrZYpXs'
 print("欢迎使用胡炎凯开发的自动图片无损压缩工具。请输入你要转换的图片地址路径,注意必须是绝对地址：")
 
 # path = input()  # 图片存放的路径
-path = r"C:\Users\micro\Desktop\医院环境图片2"
+path = r"C:\Users\micro\Desktop\新建文件夹"
 print(path)
 for dirpath, dirs, files in os.walk(path):
     for file in files:
