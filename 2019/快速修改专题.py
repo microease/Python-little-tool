8c458c4aef3d123630836d3e10f7427ce66ab06f
浮生若梦 <microease@163.com>
2020-05-01 10:41:33 +0800

    add

--- 2019/快速修改专题.py
+++ 2019/快速修改专题.py
@@ -10,15 +10,15 @@ import codecs
 
 def run():
     # 第一步：找到要修改的文件并判断文件是否存在
-    file_path = find_file("C:\\Users\micro\Desktop\美丽返场\\")  # 此处填写文件夹路径
+    file_path = find_file("C:\\Users\micro\Desktop\zonghexiongbu\\")  # 此处填写文件夹路径
     # 第二步：修改文件1：在body前加调用JS代码
     # 第三步：所有的商务通链接加nofollow
     # 第四步 删除headfoot.css
     # 第四步：读取TDK和设置的文件夹名，替换原本的title以前的字符
-    title = "激情4月·美力新生-美丽返场季_深圳非凡医疗美容医院"
-    keywords = "美丽返场季,非凡价格表,深圳非凡医疗美容医院活动"
-    description = "积分翻倍，新品体验。美力膨胀金，充值优惠！春生万物，颜值复苏，蛰伏一季的美丽，就此绽放。让美力新生，让我们与美同行！深圳非凡医疗美容医院美丽热线：0755-82281088"
-    new_dir_name = "202004mlfc"
+    title = "隆胸_胸部整形_深圳非凡医疗美容医院"
+    keywords = "胸部整形、大小胸、胸部下垂、隆胸"
+    description = "非凡胸部整形团队针对个人胸部情况及个人要求，科学设计美胸手术方案，进行针对性治疗。对平胸、胸部下垂、大小胸、巨乳等胸部问题进行矫正治疗或塑形。塑造自然动感胸部曲线，让求美者的胸部变得自然美观。美丽热线：0755-82281088"
+    new_dir_name = "202004zhxbzx"
     file_path = modify_file(file_path, new_dir_name, title, keywords, description)
     # 第五步：更换编码为GB2312
     file_path = convert_to_gb2312(file_path)
@@ -52,7 +52,7 @@ def modify_file(file_path, new_dir_name, title, keywords, description):
     new_str_5 = '''<meta charset="gb2312">\n<base href="http://m.szffmr.com/zhuanti/%s/">''' % new_dir_name
     old_str_6 = '''<meta http-equiv="X-UA-Compatible" content="ie=edge">'''
     new_str_6 = '''<meta http-equiv="X-UA-Compatible" content="ie=edge">\n<meta name="format-detection" content="telephone=no" />\n<meta content="yes" name="apple-mobile-web-app-capable" />\n<meta content="black" name="apple-mobile-web-app-status-bar-style" />'''
-    old_str_7 = '''<title>抽奖专题</title>'''
+    old_str_7 = '''<title>Document</title>'''
     new_str_7 = '''<title>%s</title>\n<meta name="keywords" content="%s">\n<meta name="description" content="%s">''' % (
         title, keywords, description)
     file_data = ""
